2b7588574f66235b8a579677069b2fff17903b3a
Rajsekhor Saikia <raj.saikia.rick333@gmail.com>
2022-10-20 18:23:35 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,95 +1,26 @@
-// sort seat based on cgpa
-// 50 seats each subject
-//Assign electives to each student
-//Create an excel sheet of those electives
+const express=require("express");
+const server=express();
+const memesList=require("./memeResource")
 
-let parser = require('simple-excel-to-json');
-let doc = parser.parseXls2Json('./Assignment.xlsx')[0]; 
-const json2xls=require("json2xls");
-const fs= require("fs");
-
-doc.sort(function(a,b){
-    return  b.CGPA - a.CGPA;
+server.get("/meme",(request,response)=>{
+    const list=[...memesList];
+    const randomIndex=Math.floor(Math.random()*list.length);
+    console.log(list[randomIndex])
+    response.send(`<img src="${list[randomIndex]}"/>`);
 })
-let fwt=0;
-let uiux=0;
-let its=0;
-let erp=0;
-console.log(doc)
 
-
-
-const ElectiveDocument=doc.map((student) => {
-    if(student.OPTION_1 =="Fundamentals of Web Technologies" && fwt<50){
-            student.ELECTIVES="Fundamentals of Web Technologies";
-            fwt+=1;
-    }
-    else if(student.OPTION_1 =="User Interface/User Experience (UI/UX) Design" && uiux<50){
-        student.ELECTIVES=  "User Interface/User Experience (UI/UX) Design";
-        uiux+=1;
-    }
-    else if(student.OPTION_1 =="Internet, Technology and Society" && its<50){
-        student.ELECTIVES=  "Internet, Technology and Society";
-        its+=1;
-    }
-    else if(student.OPTION_1 =="Enterprise Resource Planning" && erp<50){
-        student.ELECTIVES=  "Enterprise Resource Planning";
-        erp+=1;
-    }
-    else if(student.OPTION_2 =="Fundamentals of Web Technologies" && fwt<50){
-        student.ELECTIVES="Fundamentals of Web Technologies";
-        fwt+=1;
-    }
-    else if(student.OPTION_2 =="User Interface/User Experience (UI/UX) Design" && uiux<50){
-        student.ELECTIVES=  "User Interface/User Experience (UI/UX) Design";
-        uiux+=1;
-    }
-    else if(student.OPTION_2 =="Internet, Technology and Society" && its<50){
-        student.ELECTIVES=  "Internet, Technology and Society";
-        its+=1;
-    }
-    else if(student.OPTION_2 =="Enterprise Resource Planning" && erp<50){
-        student.ELECTIVES=  "Enterprise Resource Planning";
-        erp+=1;
-    }
-    else if(student.OPTION_3 =="Fundamentals of Web Technologies" && fwt<50){
-        student.ELECTIVES="Fundamentals of Web Technologies";
-        fwt+=1;
-    }
-    else if(student.OPTION_3 =="User Interface/User Experience (UI/UX) Design" && uiux<50){
-        student.ELECTIVES=  "User Interface/User Experience (UI/UX) Design";
-        uiux+=1;
-    }
-    else if(student.OPTION_3 =="Internet, Technology and Society" && its<50){
-        student.ELECTIVES=  "Internet, Technology and Society";
-        its+=1;
-    }
-    else if(student.OPTION_3 =="Enterprise Resource Planning" && erp<50){
-        student.ELECTIVES=  "Enterprise Resource Planning";
-        erp+=1;
-    }
-    else if(student.OPTION_4 =="Fundamentals of Web Technologies" && fwt<50){
-        student.ELECTIVES="Fundamentals of Web Technologies";
-        fwt+=1;
-    }
-    else if(student.OPTION_4 =="User Interface/User Experience (UI/UX) Design" && uiux<50){
-        student.ELECTIVES=  "User Interface/User Experience (UI/UX) Design";
-        uiux+=1;
-    }
-    else if(student.OPTION_4 =="Internet, Technology and Society" && its<50){
-        student.ELECTIVES=  "Internet, Technology and Society";
-        its+=1;
-    }
-    else if(student.OPTION_4 =="Enterprise Resource Planning" && erp<50){
-        student.ELECTIVES=  "Enterprise Resource Planning";
-        erp+=1;
-    }
-    else{
-        student.ELECTIVE="Unavailable"
-    }
-    return student;
+server.get("/meme/:id",(request,response)=>{
+    const list=[...memesList];
+    const num=request.params.id
+    console.log(list[num])
+    response.send(`<img src="${list[num]}"/>`);
 })
-console.log(ElectiveDocument)
-let excelDocument = json2xls(ElectiveDocument);
-fs.writeFileSync("Electives.xlsx", excelDocument, "binary")
 
+server.get("/",(request, response) => {
+    response.send("<h1>Hello World!!!</h1>")
+})
+
+
+server.listen(3000,()=>{
+    console.log("Server started on port 3000")
+})
